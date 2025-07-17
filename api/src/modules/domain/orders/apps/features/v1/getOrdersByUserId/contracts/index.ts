26aee85c08fc7e10e415b8852891593c45dd57fa
Kishor Naik <kishor.naik011.net@gmail.com>
2025-07-17 23:23:23 +0530

    feat: Add DTOs for GetOrdersByUserId request and response

---
+++ api/src/modules/domain/orders/apps/features/v1/getOrdersByUserId/contracts/index.ts
@@ -0,0 +1,24 @@
+import { IsSafeString } from "@kishornaik/utils";
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsString, IsUUID } from "class-validator";
+
+// #region Request Dto
+export class GetOrdersByUserIdRequestDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsSafeString({ message: 'Name must not contain HTML or JavaScript code' })
+  @IsUUID()
+  @Type(() => String)
+  userId:string;
+}
+// #endregion
+
+// #region Response Dto
+export class GetOrdersByUserIdResponseDto {
+  id: string;
+  status: string;
+  amount: number;
+  userId: string;
+  date: Date;
+}
+// #endregion
